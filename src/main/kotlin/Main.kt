ecd4e2b6992a989eb10b5ace7132fa84f4312216
mvciek <mpeciakk@gmail.com>
2022-12-03 22:29:48 +0100

    drawing quad using plain lwjgl (get rid of gln as it was causing some rendering problems)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
-import gln.*
+import org.lwjgl.opengl.ARBVertexArrayObject.glBindVertexArray
+import org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays
+import org.lwjgl.opengl.GL15.*
+import org.lwjgl.opengl.GL20.*
 import org.lwjgl.system.MemoryUtil
 import shader.Shader
+import java.nio.FloatBuffer
+
+
+
 
 class TestShader : Shader(
     """
@@ -35,37 +42,53 @@ fun main(args: Array<String>) {
     window.create()
 
     val vertices = floatArrayOf(
-        0.0f, 0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+        0.5f, -0.5f, 0.0f,
+        0.5f,  0.5f, 0.0f,
     )
 
-    val vao = gl.genVertexArrays()
-    vao.bound {
-        val vbo = gl.genBuffers()
-
-        vbo.bound(BufferTarget.ARRAY) {
-            data(MemoryUtil.memAllocFloat(vertices.size).put(vertices).flip())
-            gl.vertexAttribPointer(0, 3, VertexAttrType.FLOAT, false, 0, 0)
-        }
-    }
+    val indices = intArrayOf(
+        0, 1, 3,
+        3, 1, 2,
+    )
 
     val shader = TestShader()
 
+    val verticesBuffer = MemoryUtil.memAllocFloat(vertices.size)
+    verticesBuffer.put(vertices).flip()
+
+    val vao = glGenVertexArrays();
+    glBindVertexArray(vao);
+
+    val vbo = glGenBuffers();
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+
+    val idxVbo = glGenBuffers();
+    val indicesBuffer = MemoryUtil.memAllocInt(indices.size);
+    indicesBuffer.put(indices).flip();
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxVbo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
     while (!window.shouldClose) {
-        window.update()
-
-        gl.clear(ClearBufferMask.COLOR_DEPTH_BUFFER_BIT)
-        gl.clearColor(0.0f, 0.0f, 1.0f, 0.0f)
-
         shader.start()
-        vao.bound {
-            gl.enableVertexAttribArray(0)
 
-            gl.drawArrays(DrawMode.TRIANGLES, 0, 3)
+        glBindVertexArray(vao);
+        glEnableVertexAttribArray(0);
 
-            gl.disableVertexAttribArray(0)
-        }
+        glDrawElements(GL_TRIANGLES, indices.size, GL_UNSIGNED_INT, 0);
+
+        glDisableVertexAttribArray(0);
+        glBindVertexArray(0);
+
         shader.stop()
+
+        window.update()
     }
 }
--- src/main/kotlin/shader/Shader.kt
+++ src/main/kotlin/shader/Shader.kt
@@ -1,21 +1,15 @@
 package shader
 
 import Destroyable
-import glm_.mat4x4.Mat4d
-import gln.ShaderType
-import gln.gl
-import gln.identifiers.GlProgram
-import gln.identifiers.GlShader
 import org.joml.Matrix4f
-import org.joml.Vector3f
-import org.joml.Vector3i
 import org.lwjgl.BufferUtils
+import org.lwjgl.opengl.GL20.*
 import kotlin.system.exitProcess
 
 abstract class Shader(shader: String) : Destroyable {
-    private var program: GlProgram
-    private var vertex: GlShader
-    private var fragment: GlShader
+    private var program: Int
+    private var vertex: Int
+    private var fragment: Int
     private val locationCache: MutableMap<String, Int> = HashMap()
     private var matrixBuffer = BufferUtils.createFloatBuffer(16)
 
@@ -27,52 +21,72 @@ abstract class Shader(shader: String) : Destroyable {
             #version 400 core
             #define VERTEX
             ${glslPreprocessor.process(shader)}
-            """.trim(), ShaderType.VERTEX_SHADER
+            """.trim(), GL_VERTEX_SHADER
         )
         fragment = loadShader(
             """
             #version 400 core
             #define FRAGMENT
             ${glslPreprocessor.process(shader)}
-            """.trim(), ShaderType.FRAGMENT_SHADER
+            """.trim(), GL_FRAGMENT_SHADER
         )
 
-        program = gl.createProgram()
-        program.attach(vertex)
-        program.attach(fragment)
-        bindAttributes()
-        program.link()
-        program.validate()
+        program = glCreateProgram()
+
+        glAttachShader(program, vertex);
+        glAttachShader(program, fragment);
+        bindAttributes();
+        glLinkProgram(program);
+        glValidateProgram(program);
+
+//        program = gl.createProgram()
+//        glError("createProgram")
+//        program.attach(vertex)
+//        program.attach(fragment)
+//        glError("attach")
+//        bindAttributes()
+//        glError("bindAttributes")
+//        program.link()
+//        glError("link")
+//        program.validate()
+//        glError("validate")
+
+
     }
 
-    private fun loadShader(source: String, type: ShaderType): GlShader {
-        val shader = gl.createShader(type)
+    private fun loadShader(source: String, type: Int): Int {
+        val shader = glCreateShader(type)
 
-        shader.source(source)
-        shader.compile()
+//        shader.source(source)
+//        shader.compile()
 
-        if (!shader.compileStatus) {
-            println("Could not compile shader!")
-            println(shader.infoLog)
-            exitProcess(-1)
+        glShaderSource(shader, source);
+        glCompileShader(shader);
+
+        if (glGetShaderi(shader, GL_COMPILE_STATUS) == GL_FALSE) {
+            println(glGetShaderInfoLog(shader, 500));
+            println("Could not compile shader!");
+            exitProcess(-1);
         }
 
         return shader
     }
 
     fun start() {
-        program.use()
+//        program.use()
+        glUseProgram(program)
     }
 
     fun stop() {
-        program.unuse()
+//        program.unuse()
+        glUseProgram(0)
     }
 
     private fun getUniformLocation(name: String): Int {
         return if (locationCache.containsKey(name)) {
             locationCache[name]!!
         } else {
-            val location = program.getUniformLocation(name)
+            val location = glGetUniformLocation(program, name)
             locationCache[name] = location
             location
         }
@@ -90,45 +104,45 @@ abstract class Shader(shader: String) : Destroyable {
 //        loadMatrix("viewMatrix", matrix)
     }
 
-    fun loadFloat(name: String, value: Float) {
-        program.programUniform(getUniformLocation(name), value)
-    }
-
-    fun loadInt(name: String, value: Int) {
-        program.programUniform(getUniformLocation(name), value)
-    }
-
-    fun loadVector(name: String, vector: Vector3f) {
-        program.programUniform(getUniformLocation(name), vector.x, vector.y, vector.z)
-    }
-
-    fun loadVector(name: String, vector: Vector3i) {
-        program.programUniform(getUniformLocation(name), vector.x, vector.y, vector.z)
-    }
-
-    fun loadBoolean(name: String, value: Boolean) {
-        program.programUniform(getUniformLocation(name), if (value) 1 else 0)
-    }
-
-    fun loadMatrix(name: String, matrix: Mat4d) {
-//        matrixBuffer = matrix.get(matrixBuffer)
-        program.programUniform(getUniformLocation(name), matrix)
-//        program.program
-//        matrixBuffer.clear()
-    }
+//    fun loadFloat(name: String, value: Float) {
+//        program.programUniform(getUniformLocation(name), value)
+//    }
+//
+//    fun loadInt(name: String, value: Int) {
+//        program.programUniform(getUniformLocation(name), value)
+//    }
+//
+//    fun loadVector(name: String, vector: Vector3f) {
+//        program.programUniform(getUniformLocation(name), vector.x, vector.y, vector.z)
+//    }
+//
+//    fun loadVector(name: String, vector: Vector3i) {
+//        program.programUniform(getUniformLocation(name), vector.x, vector.y, vector.z)
+//    }
+//
+//    fun loadBoolean(name: String, value: Boolean) {
+//        program.programUniform(getUniformLocation(name), if (value) 1 else 0)
+//    }
+//
+//    fun loadMatrix(name: String, matrix: Mat4d) {
+////        matrixBuffer = matrix.get(matrixBuffer)
+//        program.programUniform(getUniformLocation(name), matrix)
+////        program.program
+////        matrixBuffer.clear()
+//    }
 
     protected abstract fun bindAttributes()
 
     protected fun bindAttribute(attribute: Int, name: String) {
-        program.bindAttribLocation(attribute, name)
+        glBindAttribLocation(program, attribute, name)
     }
 
     override fun destroy() {
         stop()
-        program.detach(vertex)
-        program.detach(fragment)
-        vertex.delete()
-        fragment.delete()
-        program.delete()
+//        program.detach(vertex)
+//        program.detach(fragment)
+//        vertex.delete()
+//        fragment.delete()
+//        program.delete()
     }
 }
