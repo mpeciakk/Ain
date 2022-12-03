63e9d87aa17be48f5264ac70f50b206f7292e7b7
mvciek <mpeciakk@gmail.com>
2022-12-03 21:16:16 +0100

    basic glfw window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val window = Window(800, 600, "Ain engine")
+    window.create()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    while (true) {
+        window.update()
+    }
 }
---
+++ src/main/kotlin/Window.kt
@@ -0,0 +1,59 @@
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.system.MemoryStack
+
+class Window(width: Int, height: Int, title: String) : Destroyable {
+    private val id: Long
+
+    var shouldClose: Boolean
+        get() = glfwWindowShouldClose(id)
+        set(value) = glfwSetWindowShouldClose(id, value)
+
+    init {
+        GLFWErrorCallback.createPrint(System.err).set()
+
+        if (!glfwInit()) {
+            throw IllegalStateException("Unable to initialize GLFW")
+        }
+
+        id = glfwCreateWindow(width, height, title, 0, 0)
+    }
+
+    fun create() {
+        glfwDefaultWindowHints()
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+
+        if (id == 0L) {
+            throw RuntimeException("Failed to create GLFW window!")
+        }
+
+        MemoryStack.stackPush().use { stack ->
+            val pWidth = stack.mallocInt(1)
+            val pHeight = stack.mallocInt(1)
+            glfwGetWindowSize(id, pWidth, pHeight)
+            val videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor())!!
+            glfwSetWindowPos(
+                id,
+                (videoMode.width() - pWidth.get(0)) / 2,
+                (videoMode.height() - pHeight.get(0)) / 2
+            )
+        }
+
+        glfwMakeContextCurrent(id)
+        glfwShowWindow(id)
+
+        GL.createCapabilities()
+    }
+
+    fun update() {
+        glfwSwapBuffers(id)
+        glfwPollEvents()
+    }
+
+    override fun destroy() {
+        glfwDestroyWindow(id)
+        glfwTerminate()
+    }
+}
